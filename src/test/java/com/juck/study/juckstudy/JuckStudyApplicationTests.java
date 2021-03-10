6b6ec8b307d2ccbd959457f2eaf4afda32d9e085
zhangchao <zhangchao@pekon.com>
2021-03-10 10:51:51 +0800

    init
    (cherry picked from commit bf0ce88d8e891752058767ad5222dd2b073133ad)

---
+++ src/test/java/com/juck/study/juckstudy/JuckStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juck.study.juckstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuckStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
